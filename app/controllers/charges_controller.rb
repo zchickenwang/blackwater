854888bcc9b01c9fe3e202735cc26784efa47f9d
Z Wang <zzwang@berkeley.edu>
2016-10-24 14:35:43 -0700

    adfs

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -22,5 +22,7 @@ class ChargesController < ApplicationController
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_charge_path
+
+    redirect_to cart_clear_path
     end
 end
