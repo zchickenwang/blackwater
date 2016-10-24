e6a47237785a5f98529122cc54d777a7de27b503
Z Wang <zzwang@berkeley.edu>
2016-10-24 15:00:57 -0700

    redirect after payment

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -18,9 +18,11 @@ class ChargesController < ApplicationController
         :description => 'Rails Stripe customer',
         :currency    => 'usd'
       )
+      redirect_to :controller => 'cart', :action => 'clearCart' 
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to new_charge_path
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to new_charge_path
+      end
     end
 end
